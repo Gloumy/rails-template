685308dc072bdc6036ca59c0018370a4eccf25d2
Simon <simon@cornet.dev>
2020-04-15 17:37:04 +0200

    add haml-rails, convert layout

--- template.rb
+++ template.rb
@@ -1,4 +1,20 @@
 # Initial commit
+puts "--- GIT ---"
 git :init
 git :add => '-A'
 git :commit => '-qm "initial commit"'
+puts "--- /GIT ---"
+
+# HAML
+puts "--- HAML ---"
+gem 'haml-rails'
+gem 'html2haml', :group => :development
+require 'html2haml'
+puts "convert layout to HAML"
+rails_command "generate haml:application_layout convert"
+puts "remove old ERB layout"
+run "rm -rf app/views/layouts/application.html.erb"
+git :add => '-A'
+git :commit => '-qm "add HAML support"'
+puts "--- /HAML ---"
+
