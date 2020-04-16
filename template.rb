b20db4ccd2b3e8f9903ab2186bb994b4023dd5fd
Simon <simon@cornet.dev>
2020-04-16 11:40:07 +0200

    foundation 6

--- template.rb
+++ template.rb
@@ -1,20 +1,57 @@
 # Initial commit
-puts "--- GIT ---"
+say "--- GIT ---"
 git :init
 git :add => '-A'
 git :commit => '-qm "initial commit"'
-puts "--- /GIT ---"
+say "--- /GIT ---"
 
 # HAML
-puts "--- HAML ---"
+say "--- HAML ---"
 gem 'haml-rails'
 gem 'html2haml', :group => :development
 require 'html2haml'
-puts "convert layout to HAML"
+say "convert layout to HAML"
 rails_command "generate haml:application_layout convert"
-puts "remove old ERB layout"
+say "remove old ERB layout"
 run "rm -rf app/views/layouts/application.html.erb"
 git :add => '-A'
 git :commit => '-qm "add HAML support"'
-puts "--- /HAML ---"
+say "--- /HAML ---"
 
+# Foundation 6
+after_bundle do
+  say "--- Foundation 6 ---"
+  run "yarn add jquery foundation-sites motion-ui"
+  inject_into_file 'config/webpack/environment.js', "const webpack = require(\"webpack\")" ,after: "const vue = require('./loaders/vue')\n"
+  inject_into_file 'config/webpack/environment.js', after: "environment.loaders.prepend('vue', vue)\n" do <<~EOF
+    environment.plugins.append("Provide", new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }))
+
+    EOF
+  end
+  run "mkdir app/javascript/src"
+  run "touch app/javascript/src/application.scss"
+  inject_into_file "app/javascript/src/application.scss" do <<~EOF
+    @import '~foundation-sites/dist/css/foundation';
+    @import '~motion-ui/motion-ui';
+
+    EOF
+  end
+
+  inject_into_file "app/javascript/packs/application.js", after: "require(\"channels\")\n" do <<~EOF
+    import "foundation-sites"
+    require("src/application")
+
+    $(document).on('turbolinks:load', function () {
+      $(document).foundation()
+    });
+
+    EOF
+  end
+  gsub_file "app/views/layouts/application.html.haml", /stylesheet_link_tag/, 'stylesheet_pack_tag'
+  git :add => '-A'
+  git :commit => '-qm "add Foundation 6"'
+  say "--- /Foundation 6 ---"
+end
